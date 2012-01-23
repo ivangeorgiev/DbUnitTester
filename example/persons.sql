1aa0e89152955a4c41ba35eb2567e71645b2d9ac
Ivan Georgiev <ivan.georgiev@gmail.com>
2012-01-23 22:28:00 +0200

    Example data created for persons and fix_persons tables.

---
+++ example/persons.sql
@@ -0,0 +1,17 @@
+CREATE TABLE fix_persons (
+   person_id INTEGER,
+   first_name VARCHAR(32),
+   last_name VARCHAR(32)
+);
+INSERT INTO fix_persons VALUES(1, 'Joan', 'Simpson');
+
+
+
+
+CREATE TABLE persons (
+   person_id INTEGER,
+   first_name VARCHAR(32),
+   last_name VARCHAR(32)
+);
+INSERT INTO persons VALUES(1, 'Joan', 'Simpson');
+INSERT INTO persons VALUES(2, 'Harald', 'Pilden');
